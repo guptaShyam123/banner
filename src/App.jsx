74c02d8fc4b8e1240452d7e5deb735bc4e84cdd9
Shyam gupta <shyam.gupta@iskylar.in>
2022-08-02 18:23:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Banner from './Components/Banner'
 import Phone from './Components/Phone'
+import Baby from './Components/Baby'
 
 const App = () => {
   return (
-   <Phone/>
+   <Baby/>
   )
 }
 
